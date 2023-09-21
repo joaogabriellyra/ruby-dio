8a32130b1a0efb0c5d4bd460f05c43a59a50cde6
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-09-21 02:40:01 -0300

    First commit

---
+++ conceitos_de_ruby/tipos-de-dados/tipos_de_dados.rb
@@ -0,0 +1,16 @@
+# int, float, boolean, string, array
+# symbol
+# hash
+puts 'oi'.upcase
+puts 'OI'.downcase
+puts "saodkaosdka".reverse
+puts "gabriel".capitalize
+gabriel = "Gabriel"
+puts "meu nome é #{gabriel}"
+
+puts "qual o teu nome, jogador?"
+nome = gets.chomp
+puts nome
+puts "qual a tua idade, bença?: "
+idade = gets.chomp.to_i
+
